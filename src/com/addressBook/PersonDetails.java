1cde6fb882cffd8c4b1efff630fb7c9770283837
Renuka Sawale <rrsuryawanshi11@gmail.com>
2021-05-09 17:05:10 +0530

    Added Welcome message

---
+++ src/com/addressBook/PersonDetails.java
@@ -0,0 +1,23 @@
+package com.addressBook;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class PersonDetails {
+   /* public void operations() {
+        List<String> list1 = new ArrayList<String>();
+        list1.add("Renuka");
+        list1.add("Sawale");
+        list1.add("Hinjewadi");
+        list1.add("Pune");
+        list1.add("Maharastra");
+        list1.add("410050");
+        list1.add("8104232323");
+        list1.add("reenusurya11@gmail.com");
+        Iterator itr = list1.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+    }*/
+}
